2f411a1c173f6c8e72334d9c57aa77fdc1a274c7
kat <katwang7749@gmail.com>
2025-12-24 14:29:46 -0500

    there is a strange glitch (something to do with strings and objects and randomFood
    i will fix it later because i want to choose peace and eat lunch

--- src/main.js
+++ src/main.js
@@ -25,58 +25,75 @@ loadSprite("plate", "sprites/plate.png");
 loadSprite("cup", "sprites/cup.png");
 loadSprite("takeoutbag", "sprites/takeoutbag.png");
 
+
 scene ("game", () => {
-    var randomFoodNumber = randi(6);
-    
+    var randomFoodNumber = 0;
     var randomFood = "";
     var randomFoodMissing = "";
-
-    if (randomFoodNumber == 0) {
-        randomFood = "icecream_vanilla";
-        randomFoodMissing = "icecream_missing";
-    } else if (randomFoodNumber == 1) {
-        randomFood = "icecream_strawberry";
-        randomFoodMissing = "icecream_missing";
-    } else if (randomFoodNumber == 2) {
-        randomFood = "icecream_chocolate";
-        randomFoodMissing = "icecream_missing";
-    } else if (randomFoodNumber == 3) {
-        randomFood = "icecream_caramel";
-        randomFoodMissing = "icecream_missing";
-    } else if (randomFoodNumber == 4) {
-        randomFood = "donut_strawberry";
-        randomFoodMissing = "donut_missing";
-    } else if (randomFoodNumber == 5) {
-        randomFood = "donut_chocolate";
-        randomFoodMissing = "donut_missing";
-    }
-
-    // console.log(randomFoodNumber);
-    // console.log(randomFood);
-    // console.log(randomFoodMissing);
-
+    var missingItem = "";
     var missingItemX = 200;
     var missingItemY = 600;
-    var missingItemScale = 1;
+    var missingItemScale = 1;       
     var foodContainer = "";
 
-    if (randomFoodNumber == 4 || randomFoodNumber == 5) {
-        missingItemX = 175;
-        missingItemY = 650; 
-        missingItemScale = 0.6;   
-        foodContainer = "plate";
-    } else {
-        missingItemX = 135;
-        missingItemY = 625;
-        missingItemScale = 0.45;
-        foodContainer = "cup";
-    }
 
-    var missingItem = add([
-        pos(missingItemX, missingItemY),
-        sprite(randomFoodMissing),
-        scale(missingItemScale),
-    ])
+    function newFoodItem () {
+        randomFoodNumber = randi(6);    
+
+        if (randomFoodNumber == 0) {randomFood = "icecream_vanilla";}
+        if (randomFoodNumber == 1) {randomFood = "icecream_strawberry";}
+        if (randomFoodNumber == 2) {randomFood = "icecream_chocolate";}
+        if (randomFoodNumber == 3) {randomFood = "icecream_caramel";}
+        if (randomFoodNumber == 4) {randomFood = "donut_strawberry";}
+        if (randomFoodNumber == 5) {randomFood = "donut_chocolate";}
+
+        if (randomFoodNumber <= 3) {
+            randomFoodMissing = "icecream_missing";
+            missingItemX = 135;
+            missingItemY = 625;
+            missingItemScale = 0.45;
+            foodContainer = "cup";
+        } 
+        else {
+        randomFoodMissing = "donut_missing";
+            missingItemX = 175;
+            missingItemY = 650; 
+            missingItemScale = 0.6;   
+            foodContainer = "plate";
+        }
+
+        destroyAll("foodItem");
+        destroy(missingItem);
+
+        missingItem = add([
+            pos(missingItemX, missingItemY),
+            sprite(randomFoodMissing),
+            scale(missingItemScale),
+        ])
+        
+        foodItem = add([
+            pos(0, missingItemY),
+            sprite(randomFood),
+            scale(missingItemScale + 0.03),
+            area(),
+            "foodItem",
+        ])
+    }
+    
+
+    // if (randomFoodNumber == 4 || randomFoodNumber == 5) {
+    //     missingItemX = 175;
+    //     missingItemY = 650; 
+    //     missingItemScale = 0.6;   
+    //     foodContainer = "plate";
+    // } else {
+    //     missingItemX = 135;
+    //     missingItemY = 625;
+    //     missingItemScale = 0.45;
+    //     foodContainer = "cup";
+    // }
+
+
     
     // var foodContainerItem = add([
     //     pos(missingItemX - 10, missingItemY + 50),
@@ -85,14 +102,15 @@ scene ("game", () => {
     // ]) //boi just code this later
 
     var foodItemDir = 1; //right
+    var foodItemSpeed = 240;
 
     onUpdate(() => {
-        foodItem.move(foodItemDir*240, 0);
+        foodItem.move(foodItemDir*foodItemSpeed, 0);
         
-        if (foodItem.pos.x > 320) {
+        if (foodItem.pos.x > 310) {
             foodItemDir = -1; //left
         } 
-        if (foodItem.pos.x < 0) {
+        if (foodItem.pos.x < -15) {
             foodItemDir = 1; //right
         }
     })
@@ -102,13 +120,21 @@ scene ("game", () => {
     //     foodItemDir = RIGHT;
     // } 
 
-    var foodItem = add([
-        pos(0, missingItemY),
-        sprite(randomFood),
-        scale(missingItemScale + 0.03),
-        area(),
-        "foodItem",
-    ])
+    var foodItem = "";
+    newFoodItem();
+
+    onClick(() => {
+        if (Math.abs(foodItem.pos.x - missingItem.pos.x) <= 30) {
+            destroy(foodItem);
+            debug.log("yay");
+            randomFoodNumber = randi(6);
+            foodItemSpeed += 25;
+            newFoodItem();
+        } else {
+            debug.log("booo");
+        }
+    })
 })
+
 
 go("game");
